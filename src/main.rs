3f7efe55b0e2c56614741ab9346f1a185418c806
Sergey Sekret <monkeyinsight@gmail.com>
2023-09-22 17:46:18 +0200

    fetch all songs of author

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,20 @@
 use reqwest::Client;
+use async_recursion::async_recursion;
+use reqwest::ClientBuilder;
+use std::future::Future;
+use std::pin::Pin;
 use reqwest::Error;
 use regex::Regex;
+use rust_embed::RustEmbed;
 use std::collections::HashMap;
 use std::io::{copy,Cursor};
 use clap::Parser;
 use serde::{Deserialize,Serialize};
+
+#[derive(RustEmbed)]
+#[folder = "queries"]
+#[prefix = "queries/"]
+struct Asset;
 
 #[derive(Parser, Debug)]
 #[clap(name = "Get Music", author, version, about, long_about=None)]
@@ -16,17 +26,28 @@ pub struct Args {
 }
 
 #[derive(Deserialize, Debug)]
-struct Response {
-    data: Data 
+struct SongsResponse {
+    data: SongData
 }
 
 #[derive(Deserialize, Debug)]
-struct Data {
+struct ArtistResponse {
+    data: ArtistData 
+}
+
+#[derive(Deserialize, Debug)]
+struct ArtistData {
     user: User
 }
 
 #[derive(Deserialize, Debug)]
+struct SongData {
+    node: User
+}
+
+#[derive(Deserialize, Debug)]
 struct User {
+    id: String,
     uploads: Uploads
 }
 
@@ -59,14 +80,17 @@ struct Node {
 
 #[derive(Serialize, Debug)]
 struct Payload<'a> {
-    query: String,
+    query: &'a str,
     variables: Variables<'a>
 }
 
 #[derive(Serialize, Debug)]
 struct Variables<'a> {
-    lookup: Lookup<'a>,
-    orderBy: String
+    lookup: Option<Lookup<'a>>,
+    orderBy: String,
+    count: u8,
+    cursor: Option<String>,
+    id: Option<String>
 }
 
 #[derive(Serialize, Debug)]
@@ -77,437 +101,111 @@ struct Lookup<'a> {
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     let args = Args::parse();
-    let _ = get_mixcloud("otographic".to_string()/* args.artist.unwrap() */).await;
+    let data = get_data("otographic".to_string()/* args.artist.unwrap() */).await;
+    println!("{:?}", data.unwrap());
     Ok(())
 }
 
-#[derive(Debug)]
-pub struct Track {
-    name: String,
-    url: String
+#[async_recursion]
+async fn recursive_pages(
+    get_songs: String,
+    client: Client,
+    id: String,
+    cursor: Option<String>
+) -> Result<Vec<Node>, String> {
+    let variables = Variables {
+        lookup: None,
+        orderBy: "LATEST".to_string(),
+        count: 20,
+        cursor,
+        id: Some(id.to_string())
+    };
+    let payload = Payload {
+        query: &get_songs,
+        variables
+    };
+    println!("{:?}", serde_json::json!(&payload).to_string());
+    let response = client.post("https://app.mixcloud.com/graphql")
+        .header("Host", "app.mixcloud.com")
+        .json(&payload)
+        .send()
+        .await
+        .unwrap();
+
+    match response.status() {
+        reqwest::StatusCode::OK => {
+            let res = response.json::<SongsResponse>().await.unwrap();
+            println!("{:?}", res);
+            let mut uploads: Vec<Node> = res.data.node.uploads.edges.into_iter().map(|x| x.node).collect();
+            if res.data.node.uploads.pageInfo.hasNextPage {
+                let mut results = recursive_pages(get_songs, client, id.to_owned(), Some(res.data.node.uploads.pageInfo.endCursor)).await.unwrap();
+                uploads.append(&mut results);
+            }
+            Ok(uploads)
+        },
+        status => Err(format!("Error fetching request\n{}", status))
+    }
 }
 
-async fn get_mixcloud(artist: String) -> Result<(), Error> {
+async fn get_data(artist: String) -> Result<Vec<Node>, &'static str> {
     let client = Client::builder()
         .cookie_store(true)
         .user_agent("PostmanRuntime/7.32.3")
-        .build()?;
+        .build()
+        .unwrap();
 
-    let query = r#"query UserUploadsQuery(
-    $lookup: UserLookup!
-    $orderBy: CloudcastOrderByEnum
-) {
-    user: userLookup(lookup: $lookup) {
-        username
-        ...UserUploadsPage_user_7FfCv
-        id
-    }
-    viewer {
-        ...UserUploadsPage_viewer
-        id
-    }
-}
+    let get_artist = Asset::get("queries/getArtist.gql").unwrap();
 
-fragment AudioCardActions_cloudcast on Cloudcast {
-    id
-    isPublic
-    slug
-    isExclusive
-    isUnlisted
-    isScheduled
-    isDraft
-    audioType
-    isDisabledCopyright
-    owner {
-        id
-        username
-        isSubscribedTo
-        isViewer
-        affiliateUsers {
-            totalCount
-        }
-    }
-    ...AudioCardFavoriteButton_cloudcast
-    ...AudioCardRepostButton_cloudcast
-    ...AudioCardShareButton_cloudcast
-    ...AudioCardAddToButton_cloudcast
-    ...AudioCardHighlightButton_cloudcast
-    ...AudioCardBoostButton_cloudcast
-    ...AudioCardStats_cloudcast
-}
-
-fragment AudioCardActions_viewer on Viewer {
-    me {
-        uploadLimits {
-            tracksPublishRemaining
-                showsPublishRemaining
-        }
-        id
-    }
-    ...AudioCardFavoriteButton_viewer
-    ...AudioCardRepostButton_viewer
-    ...AudioCardHighlightButton_viewer
-}
-
-fragment AudioCardAddToButton_cloudcast on Cloudcast {
-    id
-    isUnlisted
-    isPublic
-}
-
-fragment AudioCardBoostButton_cloudcast on Cloudcast {
-    id
-    isPublic
-    owner {
-        id
-        isViewer
-    }
-}
-
-fragment AudioCardFavoriteButton_cloudcast on Cloudcast {
-    id
-    isFavorited
-    isPublic
-    hiddenStats
-    favorites {
-        totalCount
-    }
-    slug
-    owner {
-        id
-        isFollowing
-        username
-        isSelect
-        displayName
-        isViewer
-    }
-}
-
-fragment AudioCardFavoriteButton_viewer on Viewer {
-    me {
-        id
-    }
-}
-
-fragment AudioCardHighlightButton_cloudcast on Cloudcast {
-    id
-    isPublic
-    isHighlighted
-    owner {
-        isViewer
-        id
-    }
-}
-
-fragment AudioCardHighlightButton_viewer on Viewer {
-    me {
-        id
-        hasProFeatures
-        highlighted {
-            totalCount
-        }
-    }
-}
-
-fragment AudioCardPlayButton_cloudcast on Cloudcast {
-    id
-    restrictedReason
-    owner {
-        displayName
-        country
-        username
-        isSubscribedTo
-        isViewer
-        id
-    }
-    slug
-    isAwaitingAudio
-    isDraft
-    isPlayable
-    streamInfo {
-        hlsUrl
-        dashUrl
-        url
-        uuid
-    }
-    audioLength
-    currentPosition
-    proportionListened
-    repeatPlayAmount
-    hasPlayCompleted
-    seekRestriction
-    previewUrl
-    isExclusivePreviewOnly
-    isExclusive
-    isDisabledCopyright
-}
-
-fragment AudioCardProgress_cloudcast on Cloudcast {
-    id
-    proportionListened
-    audioLength
-}
-
-fragment AudioCardRepostButton_cloudcast on Cloudcast {
-    id
-    isReposted
-    isExclusive
-    isPublic
-    reposts {
-        totalCount
-    }
-    owner {
-        isViewer
-        isSubscribedTo
-        id
-    }
-}
-
-fragment AudioCardRepostButton_viewer on Viewer {
-    me {
-        id
-    }
-}
-
-fragment AudioCardShareButton_cloudcast on Cloudcast {
-    id
-    isUnlisted
-    isPublic
-    slug
-    description
-    audioType
-    picture {
-        urlRoot
-    }
-    owner {
-        displayName
-        isViewer
-        username
-        id
-    }
-}
-
-fragment AudioCardStats_cloudcast on Cloudcast {
-    isExclusive
-    isDraft
-    hiddenStats
-    plays
-    publishDate
-    qualityScore
-    listenerMinutes
-    owner {
-        isSubscribedTo
-        id
-    }
-    tags(country: \"GLOBAL\") {
-        tag {
-            name
-            slug
-            id
-        }
-    }
-    ...AudioCardTags_cloudcast
-}
-
-fragment AudioCardSubLinks_cloudcast on Cloudcast {
-    id
-    isExclusive
-    owner {
-        id
-        displayName
-        username
-        ...Hovercard_user
-    }
-    creatorAttributions(first: 2) {
-        totalCount
-        edges {
-            node {
-                id
-                displayName
-                username
-                ...Hovercard_user
+    let lookup = Lookup { username: &artist };
+    let variables = Variables {
+        lookup: Some(lookup),
+        orderBy: "LATEST".to_string(),
+        count: 20,
+        cursor: None,
+        id: None
+    };
+    let payload = Payload {
+        query: std::str::from_utf8(&get_artist.data.as_ref()).unwrap(),
+        variables
+    };
+    let response = client.post("https://app.mixcloud.com/graphql")
+        .header("Host", "app.mixcloud.com")
+        .json(&payload)
+        .send()
+        .await
+        .unwrap();
+    
+    match response.status() {
+        reqwest::StatusCode::OK => {
+            let get_songs = Asset::get("queries/getSongs.gql").unwrap();
+            let res = response.json::<ArtistResponse>().await.unwrap();
+            let mut uploads: Vec<Node> = res.data.user.uploads.edges.into_iter().map(|x| x.node).collect();
+            if res.data.user.uploads.pageInfo.hasNextPage {
+                let mut results = recursive_pages(std::str::from_utf8(&get_songs.data.as_ref()).unwrap().to_string(), client, res.data.user.id, Some(res.data.user.uploads.pageInfo.endCursor)).await.unwrap();
+                uploads.append(&mut results);
             }
-        }
+            Ok(uploads)
+        },
+        _other => Err("test")
     }
-}
+    
+    // let response = client.get(format!("https://www.mixcloud.com/{}/", &artist)).send().await.unwrap();
+    // match response.status() {
+    //     reqwest::StatusCode::OK => {
+    //         let body = response.text().await?;
+    //         let re = Regex::new(r#"\"name\":\"([^"]+?)\",\"audi.+?\"previewUrl\":\"(.+?)\""#).unwrap();
+    //         let mut results: Vec<Track> = vec![];
 
-fragment AudioCardTags_cloudcast on Cloudcast {
-    tags(country: \"GLOBAL\") {
-        tag {
-            name
-            slug
-            id
-        }
-    }
-}
-
-fragment AudioCardTitle_cloudcast on Cloudcast {
-    id
-    slug
-    name
-    audioType
-    audioQuality
-    isLiveRecording
-    isExclusive
-    owner {
-        id
-        username
-        ...UserBadge_user
-    }
-    creatorAttributions(first: 2) {
-        totalCount
-    }
-    ...AudioCardSubLinks_cloudcast
-    ...AudioCardPlayButton_cloudcast
-    ...ExclusiveCloudcastBadgeContainer_cloudcast
-}
-
-fragment AudioCard_cloudcast on Cloudcast {
-    id
-    slug
-    name
-    audioType
-    isAwaitingAudio
-    isDraft
-    isScheduled
-    restrictedReason
-    publishDate
-    isLiveRecording
-    isDisabledCopyright
-    owner {
-        isViewer
-        username
-        id
-    }
-    picture {
-        ...UGCImage_picture
-    }
-    ...AudioCardTitle_cloudcast
-    ...AudioCardProgress_cloudcast
-    ...AudioCardActions_cloudcast
-    ...QuantcastCloudcastTracking_cloudcast
-}
-
-fragment AudioCard_viewer on Viewer {
-    ...AudioCardActions_viewer
-    me {
-        uploadLimits {
-            tracksPublishRemaining
-            showsPublishRemaining
-        }
-        id
-    }
-}
-
-fragment ExclusiveCloudcastBadgeContainer_cloudcast on Cloudcast {
-    isExclusive
-    isExclusivePreviewOnly
-    slug
-    id
-    owner {
-        username
-        id
-    }
-}
-
-fragment Hovercard_user on User {
-    id
-}
-
-fragment QuantcastCloudcastTracking_cloudcast on Cloudcast {
-    owner {
-        quantcastTrackingPixel
-        id
-    }
-}
-
-fragment ShareAudioCardList_user on User {
-    biog
-    username
-    displayName
-    id
-    isUploader
-    picture {
-        urlRoot
-    }
-}
-
-fragment UGCImage_picture on Picture {
-    urlRoot
-    primaryColor
-}
-
-fragment UserBadge_user on User {
-    hasProFeatures
-    isStaff
-    hasPremiumFeatures
-}
-
-fragment UserUploadsPage_user_7FfCv on User {
-    id
-    displayName
-    username
-    isViewer
-    ...ShareAudioCardList_user
-    uploads(first: 10, isPublic: true, orderBy: $orderBy, audioTypes: [SHOW]) {
-        edges {
-            node {
-                ...AudioCard_cloudcast
-                id
-                __typename
-            }
-            cursor
-        }
-        pageInfo {
-            endCursor
-            hasNextPage
-        }
-    }
-}
-
-fragment UserUploadsPage_viewer on Viewer {
-    ...AudioCard_viewer
-}"#;
-        let lookup = Lookup { username: &artist };
-        let variables = Variables {
-            lookup,
-            orderBy: "LATEST".to_string()
-        };
-        let payload = Payload {
-            query: query.to_string(),
-            variables
-        };
-        let response = client.post("https://app.mixcloud.com/graphql")
-            .header("Host", "app.mixcloud.com")
-            .json(&payload)
-            .send()
-            .await?;
-
-        match response.status() {
-            reqwest::StatusCode::OK => {
-                println!("{:?}", response.json::<Response>().await?);
-            }
-            _other => println!("Unexpected status code: {}", response.status())
-        }
-        // let response = client.get(format!("https://www.mixcloud.com/{}/", &artist)).send().await.unwrap();
-        // match response.status() {
-        //     reqwest::StatusCode::OK => {
-        //         let body = response.text().await?;
-        //         let re = Regex::new(r#"\"name\":\"([^"]+?)\",\"audi.+?\"previewUrl\":\"(.+?)\""#).unwrap();
-        //         let mut results: Vec<Track> = vec![];
-
-        //         for (_, [name, url]) in re.captures_iter(&body).map(|c| c.extract()) {
-        //             results.push(Track { name: name.to_owned(), url: url.to_owned() });
-        //         }
-        //         println!("{:?}", results);
-        //         for track in results {
-        //             let _ = fetch_mixcloud(track).await;
-        //         }
-        //     }
-        //     _other => println!("Unexpected status code: {}", response.status())
-        // }
-
-        Ok(())
+    //         for (_, [name, url]) in re.captures_iter(&body).map(|c| c.extract()) {
+    //             results.push(Track { name: name.to_owned(), url: url.to_owned() });
+    //         }
+    //         println!("{:?}", results);
+    //         for track in results {
+    //             let _ = fetch_mixcloud(track).await;
+    //         }
+    //     }
+    //     _other => println!("Unexpected status code: {}", response.status())
+    // }
 }
 
 // async fn fetch_mixcloud(track: Track) -> Result<(), Error> {
